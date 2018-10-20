ef8ee3054bedc76db429478f01a9b7c4017365cc
Kati <Kati@192.168.1.14>
2018-10-21 00:27:22 +0300

    Assignment Finished

--- src/Mufasa.java
+++ src/Mufasa.java
@@ -1,4 +1,82 @@
+import static org.junit.Assert.*;
+import org.easymock.EasyMock;
+import org.junit.Test;
+import org.junit.Before;
+
 
 public class Mufasa {
+// Mock Mufasa scenarios
+	private PersonData pd;
+	private BankAccountData bad;
+	private String user;
+	private String cvc;
+	private ExtBank bank;
+	
+	@Before
+	public void setup() {
+		pd = new PersonData();
+		pd.setUserName("Jonn123");
+		bad = new BankAccountData();
+		bad.setUserName("Jonn123");
+		user = bad.getUserName();
+		cvc  = "123";
+		// Note: we have separate tests for PersonData and BankAccountData,
+		//       so we test their functionality from there.
+		//       Here we just set user name as it is a link between objects - as it is enough for a mock, and cvc
+		//       Sure, for test with a real system, we need all information populated in objects 
+		
+		//Mock create
+		bank = EasyMock.createMock(ExtBank.class);
+		//Mock set expectations
+		EasyMock.expect(bank.checkCVC(user, "123")).andReturn(true);
+		EasyMock.expect(bank.checkCVC(user, "345")).andReturn(false);
+		EasyMock.expect(bank.doTransaction(user, "123")).andReturn(true);
+		EasyMock.expect(bank.doTransaction(user, "345")).andReturn(false);
+		EasyMock.expect(bank.getRefund(user, "123")).andReturn(true);
+		EasyMock.expect(bank.getRefund(user, "345")).andReturn(false);
+		//Mock ready
+		EasyMock.replay(bank); //activating the expected behaviour
+	}
+		
+	//verifying external dependencies
+	@Test 
+	// Check positive scenario
+	public void externalCommunicationsOK() {
+		// Imitating the expected communications
+		assertTrue(bank.checkCVC(user, cvc)); 
+		assertTrue(bank.doTransaction(user, cvc)); 
+		assertTrue(bank.getRefund(user, cvc)); 
+	}
+
+	@Test 
+	// Check wrong cvc
+	public void externalWrongCVC() {
+		// Imitating the expected communications
+		cvc = "345";
+		assertFalse(bank.checkCVC(user, cvc)); 
+		// No need to further tests 
+	}
+	
+	@Test 
+	// Check wrong transaction
+	public void externalWrongTransaction() {
+		// Imitating the expected communications
+		cvc = "123"; // good one
+		assertTrue(bank.checkCVC(user, cvc)); 
+		cvc = "345"; // bad one, this makes unsuccessful transaction
+		assertFalse(bank.doTransaction(user, cvc)); 
+		// No need to test refund 
+	}
+	
+	@Test 
+	// Check wrong refund
+	public void externalWrongRefund() {
+		// Imitating the expected communications
+		cvc = "123"; // good one
+		assertTrue(bank.checkCVC(user, cvc)); 
+		assertTrue(bank.doTransaction(user, cvc)); 
+		cvc = "345"; // bad one, this makes unsuccessful refund
+		assertFalse(bank.getRefund(user, cvc)); 
+	}
 
 }
--- tests/Person.java
+++ tests/Person.java
@@ -1,12 +1,85 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Person {
+// Testing not everything, but it does most validations 
+    private PersonData pd; // Define an object
+	
+	@Before
+	public void init() {
+		pd = new PersonData(); // and create it
+	}
+	
+	
+	// Set of test cases below	
+	
+	@Test
+	// Testing First Name (No tests for Last Name)
+	public void testValidFirstName() {
+		assertTrue(pd.setFirstName("John"));
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInvalidFirstName() {
+		assertFalse(pd.setFirstName("John1"));
+		assertFalse(pd.setFirstName("Joh n"));
+		assertFalse(pd.setFirstName("Joh@n"));
 	}
+
+	@Test
+	// Testing User Name 
+	public void testValidUserName() {
+		assertTrue(pd.setUserName("Abc_123"));
+	}
+
+	@Test
+	public void testInvalidUserName() {
+		assertFalse(pd.setUserName("Abc__123"));
+		assertFalse(pd.setUserName("1234567890123456"));
+		assertFalse(pd.setUserName("Abc_@123"));
+	}
+
+
+	@Test
+	// Testing User Password 
+	public void testValidUserPassword() {
+		assertTrue(pd.setUserPassword("PaSsWoRd123","PaSsWoRd123"));
+	}
+
+	@Test
+	public void testInvalidUserPassword() {
+		assertFalse(pd.setUserPassword("PaSsWoRd123","Password123"));
+		assertFalse(pd.setUserPassword("PaSsWoRd123","PaSsWoRd125"));
+	}
+
+	// No tests for Country
+	
+	@Test
+	// Testing Birth Date
+		public void testValidBirthDate() {
+		assertTrue(pd.setBirthDate("30/03/1990"));
+	}
+
+	@Test
+	public void testInvalidBirthDate() {
+		assertFalse(pd.setBirthDate("30-03-1990"));
+		assertFalse(pd.setBirthDate("30/03/2005"));
+	}
+
+	// No tests for Phone Number
+
+	@Test
+	// Testing email 
+	public void testValidEmail() {
+		assertTrue(pd.setEmailAddress("js@mail.com","js@mail.com"));
+	}
+
+	@Test
+	public void testInvalidEmail() {
+		assertFalse(pd.setEmailAddress("js@mail.com","jd@mail.com"));
+	}
+
 
 }
